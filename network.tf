c7cba3888927e892800cb0c616ef87bc3caf9a7f
greg miller <gregomiller2020@gmail.com>
2023-05-02 19:04:41 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -2,5 +2,4 @@ module "network" {
   source  = "app.terraform.io/GREGMILLER/network/azurerm"
   version = "3.5.0"
   # insert required variables here
-  resource_group_name = "azurerm_resource_group.myresourcegroup.name"
 }
